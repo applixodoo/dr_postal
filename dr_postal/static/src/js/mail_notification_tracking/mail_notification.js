34f094cd78ed48cab0e6d59c046c5a41eda7b32f
Michael Vandesompel <mv@droggol.be>
2025-11-30 15:47:18 +0100

    Disable JS UI extensions temporarily - need v19 template selectors
    Backend webhook tracking still works fully.
    WhatsApp-style UI indicators will be added once we determine correct v19 selectors.

--- dr_postal/static/src/js/mail_notification_tracking/mail_notification.js
+++ dr_postal/static/src/js/mail_notification_tracking/mail_notification.js
@@ -1,34 +1,7 @@
 /** @odoo-module **/
 
-import { Notification } from "@mail/core/common/notification_model";
-import { patch } from "@web/core/utils/patch";
-
 /**
- * Patch the Notification model to include postal tracking fields
+ * Mail notification model patches disabled for now.
+ * Need to update for Odoo v19 model structure.
+ * Backend tracking functionality works independently.
  */
-patch(Notification.prototype, {
-    setup() {
-        super.setup(...arguments);
-        /** @type {string} */
-        this.postal_state = "none";
-        /** @type {string|null} */
-        this.postal_tracking_uuid = null;
-    },
-});
-
-/**
- * Insert postal fields into the notification record
- */
-patch(Notification, {
-    _insert(data) {
-        const notification = super._insert(...arguments);
-        if (data.postal_state !== undefined) {
-            notification.postal_state = data.postal_state;
-        }
-        if (data.postal_tracking_uuid !== undefined) {
-            notification.postal_tracking_uuid = data.postal_tracking_uuid;
-        }
-        return notification;
-    },
-});
-
--- dr_postal/static/src/js/mail_notification_tracking/postal_status.js
+++ dr_postal/static/src/js/mail_notification_tracking/postal_status.js
@@ -1,49 +1,7 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
-import { useService } from "@web/core/utils/hooks";
-
 /**
- * PostalStatus Component
- * 
- * Displays WhatsApp-style delivery status ticks for mail notifications:
- * - ✓ (grey) = Sent
- * - ✓✓ (grey) = Delivered
- * - ✓✓ (blue) = Opened/Read
- * - ✕ (red) = Bounced
+ * Postal status component disabled for now.
+ * Need to update for Odoo v19 component structure.
+ * Backend tracking functionality works independently.
  */
-export class PostalStatus extends Component {
-    static template = "dr_postal.PostalStatus";
-    static props = {
-        postalState: { type: String, optional: true },
-        className: { type: String, optional: true },
-    };
-    static defaultProps = {
-        postalState: "none",
-        className: "",
-    };
-
-    get statusClass() {
-        const state = this.props.postalState;
-        if (!state || state === "none") {
-            return "";
-        }
-        return `o_dr_postal_${state}`;
-    }
-
-    get statusTooltip() {
-        const tooltips = {
-            none: "",
-            sent: this.env._t("Sent"),
-            delivered: this.env._t("Delivered"),
-            opened: this.env._t("Opened"),
-            bounced: this.env._t("Bounced"),
-        };
-        return tooltips[this.props.postalState] || "";
-    }
-
-    get shouldShow() {
-        return this.props.postalState && this.props.postalState !== "none";
-    }
-}
-
